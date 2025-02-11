795267099aa6968fbf5e4ecfe7b35718ab1b1617
SonuFirasath <114803488+SonuFirasath@users.noreply.github.com>
2025-02-11 23:03:54 +0530

    Add files via upload

---
+++ Introduction to js/first_js.js
@@ -0,0 +1,18 @@
+alert("Hello world");
+console.log("Hey i am console......");
+
+var a = prompt("Enter your number");
+console.log("Your number is : "+ a);
+
+var istrue = confirm("Are you sure you want to leave this website.")
+
+if(istrue){
+            alert("You'r dead now .");
+}
+else{
+            alert("You'r safe");
+}
+
+
+document.body.style.backgroundColor = "green";
+document.title = "Hey how are you buddy";
