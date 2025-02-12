86b4b41a1ad67e14abdc144fb2faad7a79c6f7ca
SonuFirasath <114803488+SonuFirasath@users.noreply.github.com>
2025-02-12 23:05:26 +0530

    Add files via upload

---
+++ if else/script.js
@@ -0,0 +1,14 @@
+console.log("hello how are you.");
+
+// let age = prompt("Enter your age : ");
+
+// if (age > 18) {
+//   console.log("You can drive.");
+// } else {
+//   console.log("Your can not drive.");
+// }
+
+let marks = 9;
+
+let result = marks>10 ? "yes" : "no";
+console.log(result)
---
+++ variables in JS/script.js
@@ -0,0 +1,34 @@
+console.log("Hey i am Firasath, how are you.");
+
+var a = 5;
+var b = 8;
+var c = "harry";
+var _c = "This is valid variable";
+// var 4ad = "This is invalid variable";
+
+console.log(a + b + 10)
+console.log(typeof a,typeof b, typeof c)
+
+// const d = 4;
+// d = 89; This is not allowed because this is a constant variable
+
+let x = "Firasath";
+let y = 22;
+let z = 5.45;
+const r = false;
+let q = undefined;
+let p = null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x, typeof y, typeof z, typeof p,typeof q, typeof r);
+
+let o = {
+    name : "Firasath",
+    "job role" : 5400
+}
+
+console.log("The name type is : " + typeof o.name + "the o type is : " + typeof o)
+
+
+o.salary = 56666;
+console.log(o)
