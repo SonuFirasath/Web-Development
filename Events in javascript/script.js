c63b16709bdc31d777a9d248d67c09d929ff1d89
F G Firasath <114803488+SonuFirasath@users.noreply.github.com>
2025-02-18 23:06:53 +0530

    Add files via upload

---
+++ Events in javascript/script.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("btn")
+
+btn.addEventListener("dblclick", ()=>
+{
+    document.querySelector(".box").innerHTML = "<b> Yayy you were clicked by the user .... </b>"
+})
