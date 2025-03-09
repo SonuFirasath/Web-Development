a9a21f2359df6dd128988b87e956d5c9ab439ae9
F G Firasath <114803488+SonuFirasath@users.noreply.github.com>
2025-03-09 23:53:01 +0530

    Add files via upload

---
+++ Use context hook/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+const Navbar = ({count}) => {
+  return (
+    <>
+    <div>
+      Navbar hun meyy
+      <Button count={count}/>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
