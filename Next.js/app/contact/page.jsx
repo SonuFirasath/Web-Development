5ca4a0d050bdbed2c4e82389b191547e186e5326
F G Firasath <114803488+SonuFirasath@users.noreply.github.com>
2025-03-11 19:13:44 +0530

    Add files via upload

---
+++ Next.js/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Hey guys contact me ....
+    </div>
+  )
+}
+
+export default page
---
+++ Next.js/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      I am good how are you ?
+    </div>
+  )
+}
+
+export default page
+
