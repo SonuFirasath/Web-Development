04bd4d1ba0295006e016e1298d1f35f05a8a7f83
F G Firasath <114803488+SonuFirasath@users.noreply.github.com>
2025-03-12 23:30:36 +0530

    Add files via upload

---
+++ Server Components in next.js/my-app/app/page.js
@@ -0,0 +1,14 @@
+// "use client"
+// import { useState,useEffect } from "react";
+
+export default function Home() {
+  // const [count,setcount] = useState(0);
+  console.log("Hey i am Mikey and this is my next.js app. did you like it ?")
+  return (
+  //  <div>I am a component with value {count}
+  //  <button onClick={()=> setcount(count+1)}>Click me</button>
+  //  </div>
+  <div></div>
+   
+  );
+}
